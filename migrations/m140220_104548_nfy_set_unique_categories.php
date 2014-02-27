58f62e111d92f95e6e792f244289e6da40d07fea
Jan Was <jwas@nets.com.pl>
2014-02-27 10:50:51 +0100

    unique constraint on categories in subscriptions

---
+++ migrations/m140220_104548_nfy_set_unique_categories.php
@@ -0,0 +1,14 @@
+<?php
+
+class m140220_104548_nfy_set_unique_categories extends CDbMigration
+{
+	public function safeUp()
+	{
+		$this->execute('ALTER TABLE {{nfy_subscription_categories}} ADD CONSTRAINT {{nfy_subscription_categories}}_unique_categories UNIQUE (subscription_id, category, is_exception);');
+	}
+
+	public function safeDown()
+	{
+		$this->execute('ALTER TABLE {{nfy_subscription_categories}} DROP CONSTRAINT {{nfy_subscription_categories}}_unique_categories;');
+	}
+}
